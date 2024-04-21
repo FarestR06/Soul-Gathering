8a9d2f240b549ea7b5a7cb8fecb2047b41e35760
FarestR06 <jacobjdolunt@gmail.com>
2024-04-21 16:58:22 -0400

    Fixed soul bar not being rendered correctly

--- src/main/java/com/farestr06/soul_gathering/component/SoulComponentHelper.java
+++ src/main/java/com/farestr06/soul_gathering/component/SoulComponentHelper.java
@@ -6,6 +6,6 @@ import net.minecraft.util.math.MathHelper;
 public class SoulComponentHelper {
     public static int mappedSoulCount(PlayerEntity provider) {
         float input = ModComponents.SOUL_COMPONENT.get(provider).getSoulCount();
-        return Math.round(MathHelper.map(0f, 8192f, 0f, 20f, input));
+        return Math.round(MathHelper.map(input, 0, 8192f, 0f, 20f));
     }
 }
